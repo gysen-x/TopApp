f4729131606b9cf2a01dff0b1cdf3eb377a2fdb6
Dmitry Borisenko <brdima92@gmail.com>
2023-09-08 23:12:05 +0300

    release 2.0

--- components/global/Menu/Menu.tsx
+++ components/global/Menu/Menu.tsx
@@ -1,4 +1,6 @@
-import { FirstLevelMenuItem } from '@/interfaces/menu.interface'
+import stls from './Menu.module.css'
+import cn from 'classnames'
+import { FirstLevelMenuItem, PageItem } from '@/interfaces/menu.interface'
 import {
 	BooksIcon,
 	CoursesIcon,
@@ -7,6 +9,8 @@ import {
 } from '@/components/global/Sidebar/components'
 import { TopLevelCategory } from '@/interfaces/page.interface'
 import { getMenu } from '@/api/menu'
+
+const firstCategory = 0
 
 const firstLevelMenu: FirstLevelMenuItem[] = [
 	{
@@ -38,19 +42,57 @@ const firstLevelMenu: FirstLevelMenuItem[] = [
 export const Menu = async () => {
 	const menu = await getMenu(0)
 
-	const buildFirstLevel = () => {}
+	const buildFirstLevel = () => {
+		return (
+			<>
+				{firstLevelMenu.map(m => (
+					<div key={m.route}>
+						<a href={`/${m.route}`}>
+							<div
+								className={cn(stls.firstLevel, {
+									[stls.firstLevelActive]: m.id === firstCategory
+								})}>
+								{m.icon}
+								<span>{m.name}</span>
+							</div>
+						</a>
+						{m.id === firstCategory && buildSecondLevel(m)}
+					</div>
+				))}
+			</>
+		)
+	}
 
-	const buildSecondLevel = () => {}
+	const buildSecondLevel = (menuItem: FirstLevelMenuItem) => {
+		return (
+			<div className={stls.secondBlock}>
+				{menu.map(m => (
+					<div key={m._id.secondCategory}>
+						<div className={stls.secondLevel}>{m._id.secondCategory}</div>
+						<div
+							className={cn(stls.secondLevelBlock, {
+								[stls.secondLevelBlockOpened]: m.isOpened
+							})}>
+							{buildThirdLevel(m.pages, menuItem.route)}
+						</div>
+					</div>
+				))}
+			</div>
+		)
+	}
 
-	const buildThirdLevel = () => {}
+	const buildThirdLevel = (pages: PageItem[], route: string) => {
+		return pages.map(p => (
+			<a
+				key={p._id}
+				className={cn(stls.thirdLevel, {
+					[stls.thirdLevelActive]: false
+				})}
+				href={`/${route}/${p.alias}`}>
+				{p.category}
+			</a>
+		))
+	}
 
-	return (
-		<div>
-			<ServicesIcon />
-			<ProductsIcon />
-			<CoursesIcon />
-			<BooksIcon />
-			{menu.length}
-		</div>
-	)
+	return <div className={stls.menu}>{buildFirstLevel()}</div>
 }
--- components/global/Sidebar/Sidebar.tsx
+++ components/global/Sidebar/Sidebar.tsx
@@ -1,5 +1,5 @@
-import { Menu } from '../Menu/Menu'
 import stls from './Sidebar.module.css'
+import { Menu } from '../Menu/Menu'
 import ISidebarProps from './Sidebar.props'
 import { BooksIcon, CoursesIcon, ServicesIcon } from './components'
 import ProductsIcon from './components/icons/ProductsIcon'
